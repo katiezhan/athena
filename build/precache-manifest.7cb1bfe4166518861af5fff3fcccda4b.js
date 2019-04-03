e03c95a9df003110e21859132581a91f49048f6f
Daisy <daisyagrawal45@gmail.com>
2019-04-03 13:38:51 -0400

    initialized react and hosted on firebase

---
+++ build/precache-manifest.7cb1bfe4166518861af5fff3fcccda4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "693c74a64c120fb6c463",
+    "url": "/static/js/main.80008720.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "693c74a64c120fb6c463",
+    "url": "/static/css/main.4df20209.chunk.css"
+  },
+  {
+    "revision": "252ac720337cfc7d65c7eb45c40a659d",
+    "url": "/index.html"
+  }
+];
