7bfc2b150b61e5bc391fc232e44d5f8abb847610
Daisy <daisyagrawal45@gmail.com>
2019-04-03 13:48:13 -0400

    testing

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
+            Hello World!
           </p>
         </header>
       </div>
