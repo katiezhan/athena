2fe7566a92d1ffea9aaefe6d660c20a77c3fe51c
Daisy <daisyagrawal45@gmail.com>
2019-04-04 10:30:39 -0400

    import chart module

--- src/App.js
+++ src/App.js
@@ -78,26 +78,6 @@ class App extends Component {
         </div>
         {this.state.submitOne == false ?
           <div class = "welcome-body">
-          <Chart
-            data={[
-              {
-                label: "Series 1",
-                data: [[0, 1], [1, 2], [2, 4], [3, 2], [4, 7]]
-              },
-              {
-                label: "Series 2",
-                data: [[0, 3], [1, 1], [2, 5], [3, 6], [4, 4]]
-              }
-            ]}
-            axes={[
-              { primary: true, type: "linear", position: "bottom" },
-              { type: "linear", position: "left" }
-            ]}
-            style={{
-              width: "400px",
-              height: "300px"
-            }}
-            />
           
             <div class ="form-group">
               <div class = "row">
