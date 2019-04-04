de895a1affe931a143af6d3550e0c1d6415956ae
rqzhan@uwaterloo.ca <rqzhan@uwaterloo.ca>
2019-04-03 21:29:24 -0400

    added questions

--- src/App.js
+++ src/App.js
@@ -62,14 +62,14 @@ class App extends Component {
           <div class = "welcome-body">
             <div class ="form-group">
               <div class = "row">
-                What is question one?
+                If given minimal instruction on a certain task, how would you proceed?
               </div>
               <div class = "row">
                 <div class ="col-10" style={{padding: "2%"}}>
                   <input class="form-control" id="answerOne" placeholder="Enter your answer here" onChange={this.handleQuestionOne} value={this.state.answerOne} style ={{padding: "1%"}} required/>
                 </div>
               </div>
-              <button id="submit-button" class="btn" onClick={this.handleSubmitOne} style={{align: 'center', color: "#048D98"}} type="submit">Next</button>
+              <button id="submit-button" class="btn" onClick={this.handleSubmitOne} style={{align: 'center', color: "#048D98"}} type="submit">Submit</button>
             </div>
           </div>
         :
@@ -77,14 +77,14 @@ class App extends Component {
               <div class ="form-group">
                 Success!
                 <div class = "row">
-                   What is question two?
+                   Tell me about a time you had to work with a colleague you did not get along with. What did you do?
                 </div>
                <div class = "row">
                  <div class ="col-10" style={{padding: "2%"}}>
                    <input class="form-control" id="answerOne" placeholder="Enter your answer here" onChange={this.handleQuestionTwo} value={this.state.answerTwo} style ={{padding: "1%"}} required/>
                  </div>
                </div>
-               <button id="submit-button-2" class="btn" onClick={this.handleSubmitTwo} style={{align: 'center', color: "#048D98"}} type="submit">Next</button>
+               <button id="submit-button-2" class="btn" onClick={this.handleSubmitTwo} style={{align: 'center', color: "#048D98"}} type="submit">Submit</button>
              </div>
            </div>
         } 
